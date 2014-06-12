b9314e9e02476ed66dd4a8493ebca6ac017e416c
Rui Campos <rui.campos@gameduell.de>
2014-06-12 15:28:15 +0200

    initial commit with png loading

---
+++ media/bitmap/BitmapComponentFormat.hx
@@ -0,0 +1,12 @@
+/*
+ * Created by IntelliJ IDEA.
+ * User: rcam
+ * Date: 11/06/14
+ * Time: 12:38
+ */
+package media.bitmap;
+
+enum BitmapComponentFormat
+{
+    BitmapComponentFormatRGBA8888;
+}
