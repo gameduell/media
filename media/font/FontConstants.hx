b81b44adbe0e7d4aad813b1215516186f8c6aa19
Sven Otto <sven.otto@gameduell.de>
2014-11-12 15:24:52 +0100

    space is handled in the game engine text generator now, it can be actually more than one char code for the " " space

--- media/font/FontConstants.hx
+++ media/font/FontConstants.hx
@@ -4,7 +4,6 @@ class FontConstants
 {
     inline static public var carriageReturnCharCode: Int = 10; // "\n"
     inline static public var carriageReturnCharCodeAlt: Int = 13; // "\r"
-    inline static public var spaceCharCode: Int = 32; // " "
     inline static public var replacementCharCode: Int = 63; // "?"
     inline static public var xHeightCharCode: Int = 120; // "x"
     inline static public var maxLineTextWidth: Float = 65536.0; // 2^16 Better don't put so wide text ^
